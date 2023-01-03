e0e97fbd389d9c5a6d67f7b8ae83d1c071e28003
Mauro Vieira <mauroccvieira@gmail.com>
2023-01-03 13:43:06 -0300

    feat: initial commit

---
+++ TODO.md
@@ -0,0 +1,30 @@
+# TODO
+## Tech Stuff
+[ ] Add and configure package semantic-release
+
+[ ] Add Husky
+
+[ ] Configure conventional-commits hook with husky
+
+[ ] Add Middy
+
+## Features
+[ ] Create Account
+
+[ ] Cancel Account
+
+[ ] Deposit money
+
+[ ] Withdraw money
+
+[ ] Transfer money to another account
+
+[ ] Get Statement
+
+
+# Reminders
+[ ] Event Bridge
+
+[ ] Integration Testing
+
+[ ] Github CI
---
+++ serverless.ts
@@ -0,0 +1,42 @@
+import type { AWS } from "@serverless/typescript";
+
+const serverlessConfiguration: AWS = {
+  org: "mauroccvieira",
+  app: "serverless-boilerplate",
+  service: "serverless-boilerplate",
+  frameworkVersion: "3",
+  plugins: ["serverless-esbuild", "serverless-offline"],
+  provider: {
+    name: "aws",
+    runtime: "nodejs18.x",
+  },
+  // import the function via paths
+  functions: {
+    createAccount: {
+      handler: "src/adapter/primary/create-account-adapter.handler",
+      events: [
+        {
+          httpApi: {
+            path: "/account",
+            method: "post",
+          },
+        },
+      ],
+    },
+  },
+  package: { individually: true },
+  custom: {
+    esbuild: {
+      bundle: true,
+      minify: true,
+      sourcemap: true,
+      exclude: ["aws-sdk"],
+      packager: "pnpm",
+      target: "node18",
+      platform: "node",
+      concurrency: 10,
+    },
+  },
+};
+
+module.exports = serverlessConfiguration;
