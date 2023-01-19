4479d969c9ca62039e5135bec87d13735ff14e77
Mauro Vieira <mauroccvieira@gmail.com>
2023-01-19 09:28:22 -0300

    build: add prettier and lint-staged configuration

--- __tests__/unity/adapter/primary/create-account-adapter.test.ts
+++ __tests__/unity/adapter/primary/create-account-adapter.test.ts
@@ -1,12 +1,11 @@
 import { handler } from "@adapters/primary/create-account-adapter";
 
-
 describe("some text", () => {
-	it("some test", () => {
-		expect(handler).toBeDefined();
-	});
-  
-	it("some test 2", () => {
-		expect(handler).toBeDefined();
-	});
+  it("some test", () => {
+    expect(handler).toBeDefined();
+  });
+
+  it("some test 2", () => {
+    expect(handler).toBeDefined();
+  });
 });
