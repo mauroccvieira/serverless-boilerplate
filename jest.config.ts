8e8d2625967896a36089a704d6ae35bd4924b78b
Mauro Vieira <mauroccvieira@gmail.com>
2024-06-04 23:55:20 -0300

    ci: adds test badges to readme in main branch

--- jest.config.ts
+++ jest.config.ts
@@ -3,5 +3,6 @@ module.exports = {
     "<rootDir>/jest.unit.config.ts",
     "<rootDir>/jest.integration.config.ts",
     "<rootDir>/jest.e2e.config.ts"
-  ]
+  ],
+  coverageReporters: ["json-summary"]
 };
